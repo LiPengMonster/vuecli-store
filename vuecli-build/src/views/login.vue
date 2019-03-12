570b58bde76e8fc2dd0d34d0fd3a043a7139f6c7
LiPengMonster <524572660@qq.com>
2019-03-12 22:24:58 +0800

    删除默认文件

---
+++ vuecli-build/src/views/login.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="login-wrap" :v-show="showLogin">
+      <h3>登录</h3>
+      <p :v-show="showTishi">{{tishi}}</p>
+      <input type="text" placeholder="请输入用户名" v-model="username">
+      <input type="password" placeholder="请输入密码" v-model="password">
+      <button @click="login">登录</button>
+      <span>没有账号？马上注册</span>
+    </div>
+
+    <div class="register-wrap" v-show="showRegister">
+      <h3>注册</h3>
+      <p v-show="showTishi">{{tishi}}</p>
+      <input type="text" placeholder="请输入用户名" v-model="newUsername">
+      <input type="password" placeholder="请输入密码" v-model="newPassword">
+      <button>注册</button>
+      <span>已有账号？马上登录</span>
+    </div>
+  </div>
+</template>
+
+<style>
+.login-wrap {
+  text-align: center;
+}
+input {
+  display: block;
+  width: 250px;
+  height: 40px;
+  line-height: 40px;
+  margin: 0 auto;
+  margin-bottom: 10px;
+  outline: none;
+  border: 1px solid #888;
+  padding: 10px;
+  box-sizing: border-box;
+}
+p {
+  color: red;
+}
+button {
+  display: block;
+  width: 250px;
+  height: 40px;
+  line-height: 40px;
+  margin: 0 auto;
+  border: none;
+  background-color: #41b883;
+  color: #fff;
+  font-size: 16px;
+  margin-bottom: 5px;
+}
+span {
+  cursor: pointer;
+}
+span:hover {
+  color: #41b883;
+}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      showLogin: true,
+      showRegister: false,
+      showTishi: false,
+      tishi: '',
+      username: '123',
+      password: '',
+      newUsername: '',
+      newPassword: ''
+    }
+  },
+  methods: {
+    login () {
+      // 获取用户名，异步提交后台并处理返回数据
+    }
+  }
+}
+</script>
